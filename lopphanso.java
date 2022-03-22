021d891809f75d2c71931b7613f62a257d687996
germanduy <duyldth2106007@fpt.edu.vn>
2022-03-22 21:50:35 +0700

    ab

--- lopphanso.java
+++ lopphanso.java
@@ -1,9 +1,9 @@
-package tumauso;
+package com.company;
 
 import java.util.Scanner;
 
-public class lopphanso {
-    int a,b;
+public class lopPhanSo {
+    int a,b,c,d;
 
     public int getA() {
         return a;
@@ -13,6 +13,14 @@ public class lopphanso {
         return b;
     }
 
+    public int getC() {
+        return c;
+    }
+
+    public int getD() {
+        return d;
+    }
+
     public void setA(int a) {
         this.a = a;
     }
@@ -20,26 +28,83 @@ public class lopphanso {
     public void setB(int b) {
         this.b = b;
     }
-    boolean kiemtra(){
-        if(getB()==0) return true;
-        return false;
+
+    public void setC(int c) {
+        this.c = c;
     }
-    void rutgonphanso(){
-        System.out.println("Rút gọn phân số");
+
+    public void setD(int d) {
+        this.d = d;
     }
-    void nghichdaophanso(){
-        System.out.println("Nghịch đảo phân số");
+
+    //kiểm tra mẫu khác không
+    boolean kiemtra1(){
+        if(getB()==0){return true;}
+        else {return false;}
     }
-    void cong(){
-        System.out.println("Cộng");
+    boolean kiemtra2(){
+        if(getD()==0){return true;}
+        else {return false;}
     }
-    void tru(){
-        System.out.println("Trừ");
+    //Tìm ucln
+    int timUcln(){
+        int ucln=1;
+        if(getA()>getB()){
+            for(int i=1 ; i<=getB(); i++){
+                if(getA()%i==0 && getB()%i==0){
+                    ucln=i;
+                }
+            }
+        }else{
+            for(int i=1; i<=getA(); i++){
+                if(getA()%i==0 && getB()%i==0){
+                    ucln=i;
+                }
+            }
+        }
+        return ucln;
     }
-    void nhan(){
-        System.out.println("Nhân");
+    //rút gọn phân số
+    void rutgonPhanSo(){
+        setA(getA()/timUcln());
+        setB(getB()/timUcln());
+        System.out.println("Phân số rút gọn là: "+getA()+"/"+getB());
     }
-    void chia(){
-        System.out.println("Chia");
+    //Nghịc đảo phân số
+    void nghichdaoPhanSo(){
+        if(getA()==0){
+            System.out.println("Không có phân số nghịch đảo");
+        }else{
+            System.out.println("Phân số nghịch đảo là: "+getB()+"/"+getA());
+        }    }
+    // Tong
+    void tong(){
+        int tongtu,tongmau;
+        tongtu= getA()*getD()+getB()*getC();
+        tongmau =getB()*getD();
+        System.out.println("Tổng 2 phân số là: "+tongtu+"/"+tongmau);
     }
+    //Hieu
+    void hieu(){
+        int tongtu,tongmau;
+        tongtu= getA()*getD()-getB()*getC();
+        tongmau =getB()*getD();
+        System.out.println("Hiệu 2 phân số là: "+tongtu+"/"+tongmau);
+    }
+    //Tich
+    void tich(){
+        int tongtu,tongmau;
+        tongtu= getA()*getC();
+        tongmau =getB()*getD();
+        System.out.println("Tích 2 phân số là: "+tongtu+"/"+tongmau);
+    }
+    //Thuong
+    void thuong(){
+        int tongtu,tongmau;
+        tongtu= getA()*getD();
+        tongmau =getB()*getC();
+        System.out.println("Thương 2 phân số là: "+tongtu+"/"+tongmau);
+    }
+
+
 }
--- main.java
+++ main.java
@@ -1,21 +1,38 @@
-package tumauso;
+package com.company;
 
 import java.util.Scanner;
 
-public class main {
+public class Main {
+
     public static void main(String[] args) {
-    lopphanso duy1 = new lopphanso();
-    Scanner sc = new Scanner(System.in);
-    duy1.setA(sc.nextInt());
-    duy1.setB(sc.nextInt());
-    System.out.println("Tử số là" + duy1.getA());
-    System.out.println("Mẫu số là'" + duy1.getB());
-    if(duy1.kiemtra()){System.out.println("Mẫu số phải khác không ");}
-    duy1.rutgonphanso();
-    duy1.nghichdaophanso();
-    duy1.cong();
-    duy1.tru();
-    duy1.nhan();
-    duy1.chia();
+	// write your code here
+        lopPhanSo PhanSo = new lopPhanSo();
+        Scanner sc =  new Scanner(System.in);
+        System.out.println("Nhập phân số 1");
+        System.out.println("Tử số");
+        PhanSo.setA(sc.nextInt());
+        System.out.println("Mẫu số");
+        PhanSo.setB(sc.nextInt());
+        if(PhanSo.kiemtra1()){
+            System.out.println("Mẫu số phải khác không");
+            PhanSo.setB(sc.nextInt());
+        }
+        PhanSo.rutgonPhanSo();
+        PhanSo.nghichdaoPhanSo();
+        System.out.println("Nhập phân số 2");
+        System.out.println("Tử số");
+        PhanSo.setC(sc.nextInt());
+        System.out.println("Mẫu số");
+        PhanSo.setD(sc.nextInt());
+        if(PhanSo.kiemtra2()){
+            PhanSo.setD(sc.nextInt());
+        }
+        PhanSo.tong();
+        PhanSo.hieu();
+        PhanSo.tich();
+        PhanSo.thuong();
+
+
+
     }
 }
